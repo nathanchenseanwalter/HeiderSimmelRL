5940c4a3410b99a1b1428ac496cf38d438a08e2c
Nathaniel Chen <79379842+nathanchenseanwalter@users.noreply.github.com>
2023-04-22 13:54:52 -0700

    Avoiding

--- envs/box2d/two_agents_avoid.py
+++ envs/box2d/two_agents_avoid.py
@@ -47,6 +47,7 @@ def _get_body_bound(body):
 
 
 def _get_door(body):
+    """get the door's location"""
     vertices1 = [(body.transform * v)  \
                     for v in body.fixtures[0].shape.vertices]
     vertices2 = [(body.transform * v) \
@@ -55,7 +56,6 @@ def _get_door(body):
     return [vertices1[0], vertices2[-1]]
 
 
-"""TODO: currently only consider a rectangle w/o rotation"""
 def _get_room_bound(body):
     """get the boundary of a room (upper-left corner + bottom-right corner)"""
     x_list, y_list = [], []
@@ -75,10 +75,6 @@ def _in_room(body, room):
     return body_bound[2] >= min_x and body_bound[3] >= min_y and \
            body_bound[0] <= max_x and body_bound[1] <= max_y
 
-def _get_dist(pos1, pos2):
-    """get distance between two points"""
-    return ((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2) ** 0.5
-
 
 def _get_obs(screen, SCREEN_WIDTH, SCREEN_HEIGHT):
     string_image = pygame.image.tostring(screen, 'RGB')
@@ -86,15 +82,18 @@ def _get_obs(screen, SCREEN_WIDTH, SCREEN_HEIGHT):
                     (SCREEN_WIDTH, SCREEN_HEIGHT),'RGB')
     return(pygame.surfarray.array3d(temp_surf))
 
+def _get_dist(pos1, pos2):
+    """get distance between two points"""
+    return ((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2) ** 0.5
 
-class Guarding_v0:
-    """agent 1 attempts to get the item while agent 2 attempts to stop"""
-    def __init__(self, PPM = 20.0, 
-                       TARGET_FPS = 60,
-                       SCREEN_WIDTH = 640,
-                       SCREEN_HEIGHT = 480,
-                       TIME_STEP = 5,
-                       enable_renderer = True,
+class Avoiding_v0:
+    """agent 2 avoids agent 1, discrete action"""
+    def __init__(self, PPM=20.0, 
+                       TARGET_FPS=60,
+                       SCREEN_WIDTH=640,
+                       SCREEN_HEIGHT=480,
+                       TIME_STEP=5,
+                       enable_renderer=True,
                        random_pos=False,
                        restitution=0):
         self.PPM = PPM
@@ -109,19 +108,20 @@ class Guarding_v0:
         self.action_size = len(self.action_space)
         if self.enable_renderer:
             self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
-            pygame.display.set_caption('Guarding')
+            pygame.display.set_caption('Avoiding_v0')
             self.clock = pygame.time.Clock()
         self.room_dim = (16, 16)
         self.door_length = 3
         self.item_size = 1
-        self.agent2_pos_list = [(10, 6), (22, 6), (10, 18)]
+        self.agent1_pos_list = [(16 + 7, 12 - 7), (16 - 7, 12 - 7), (16 + 7, 12 + 7)]
+        self.agent2_pos_list = [(10, 6), (22, 6), (22, 18), (10, 18)]
         self.random_pos = random_pos
         self.restitution = restitution
 
         random.seed(1)
 
 
-    def setup(self, record_path = None):
+    def setup(self, record_path=None):
         """setup a new espisode"""
         self.world = world(gravity=(0, 0), doSleep=True)
         self.room = self.world.CreateBody(position=(16, 12))
@@ -131,8 +131,9 @@ class Guarding_v0:
              ( self.room_dim[0] / 2,  self.room_dim[1] / 2),
              ( self.room_dim[0] / 2, -self.room_dim[1] / 2),
              (-self.room_dim[0] / 2, -self.room_dim[1] / 2),
-             (-self.room_dim[0] / 2,  self.room_dim[1] / 2 - self.door_length)]
+             (-self.room_dim[0] / 2,  self.room_dim[1] / 2)]
         )
+        # item 1
         self.item_pos = (22, 18)
         # body = self.world.CreateStaticBody(position=self.item_pos)
         body = self.world.CreateDynamicBody(position=self.item_pos)
@@ -140,14 +141,23 @@ class Guarding_v0:
         self.item = body
         self.agents = []
         # agent 1
-        body = self.world.CreateDynamicBody(position=(16 + 4, 12 - 4))
-        body.CreateCircleFixture(radius=1, density=1, friction=0.3, restitution=1)
-        self.agents.append(body)
+        if self.random_pos:
+            x, y = random.choice(self.agent1_pos_list)
+            body = self.world.CreateDynamicBody(position=(x, y))
+            body.CreateCircleFixture(radius=1, density=1, friction=0.3, restitution=self.restitution)
+            self.agents.append(body)
+            self.trajectories = [[(x, y)]]
+        else:
+            body = self.world.CreateDynamicBody(position=(16 + 4, 12 - 4))
+            body.CreateCircleFixture(radius=1, density=1, friction=0.3, restitution=self.restitution)
+            self.agents.append(body)
+            self.trajectories = [[(16 + 4, 12 - 4)]]
         # agent 2
         x, y = random.choice(self.agent2_pos_list)
         body = self.world.CreateDynamicBody(position=(x, y))
-        body.CreateCircleFixture(radius=1, density=1, friction=0.3, restitution=1)
+        body.CreateCircleFixture(radius=1, density=1, friction=0.3)
         self.agents.append(body)
+        self.trajectories.append([(x, y)])
         self.steps = 0
         self.running = False
         self.video = None
@@ -162,33 +172,12 @@ class Guarding_v0:
     def start(self):
         """start the episode"""
         self.running = True
-        # fx, fy = -1200.0, 1200.0
-        # f = self.agents[0].GetWorldVector(localVector=(fx, fy))
-        # p = self.agents[0].GetWorldPoint(localPoint=(0.0, 0.0))
-        # self.agents[0].ApplyForce(f, p, True)
-        # self.world.Step(1.0 / self.TARGET_FPS, 10, 10)
+        fx, fy = -1200.0, 1200.0
+        f = self.agents[0].GetWorldVector(localVector=(fx, fy))
+        p = self.agents[0].GetWorldPoint(localPoint=(0.0, 0.0))
+        self.agents[0].ApplyForce(f, p, True)
+        self.world.Step(1.0 / self.TARGET_FPS, 10, 10)
         self.agents_pos = [_get_world_pos(agent) for agent in self.agents]
-
-
-    def send_action_nonphysical(self, agent_id, action):
-        """send action to an agent"""
-        x, y = self.agents_pos[agent_id]
-        vel = 2.5 / self.TARGET_FPS
-        if action == 'up':
-            y += vel
-        elif action == 'down':
-            y -= vel
-        elif action == 'left':
-            x -= vel
-        elif action == 'right':
-            x += vel
-        elif action == 'stop':
-            pass
-        else:
-            raise ValueError('ERROR: invalid action!')
-        self.world.DestroyBody(self.agents[agent_id])
-        self.agents[agent_id] = self.world.CreateDynamicBody(position=(x, y), angle=0)
-        self.agents[agent_id].CreateCircleFixture(radius=1, density=1, friction=0.3)
 
 
     def send_action(self, agent_id, action):
@@ -215,6 +204,15 @@ class Guarding_v0:
         elif action == 'downright':
             fx += df
             fy -= df
+        elif action == 'stop':
+            x, y = self.agents_pos[agent_id]
+            self.world.DestroyBody(self.agents[agent_id])
+            self.agents[agent_id] = self.world.CreateDynamicBody(position=(x, y), angle=0)
+            if agent_id == 0:
+                self.agents[agent_id].CreateCircleFixture(radius=1, density=1, friction=0.3, restitution=self.restitution)
+            else:
+                self.agents[agent_id].CreateCircleFixture(radius=1, density=1, friction=0.3)
+            return
         else:
             print('ERROR: invalid action!')
         f = self.agents[agent_id].GetWorldVector(localVector=(fx, fy))
@@ -228,28 +226,28 @@ class Guarding_v0:
         self.world.Step(self.TIME_STEP, 10, 10)
         self.world.ClearForces()
         self.agents_pos = [_get_world_pos(agent) for agent in self.agents]
-        self.item_pos = [_get_world_pos(self.item)]
+        for agent_id, agent_pos in enumerate(self.agents_pos):
+            self.trajectories[agent_id].append((agent_pos[0], agent_pos[1]))
         self.running = not self.terminal()
         self.render()
 
 
     def get_obs(self):
         """get observation"""
-        single_obs = imresize(_get_obs(self.screen, 
-                            self.SCREEN_WIDTH, self.SCREEN_HEIGHT), 
-                            (self.obs_dim[1], self.obs_dim[2])).transpose([2, 0, 1]).reshape((-1))
-        return [single_obs, single_obs]
+        return [imresize(_get_obs(self.screen, 
+                        self.SCREEN_WIDTH, self.SCREEN_HEIGHT), 
+                        (self.obs_dim[1], self.obs_dim[2])).transpose([2, 0, 1]).reshape((-1))
+                for _ in range(2)]
 
 
     def terminal(self):
         """check if the goal is achieved"""
-        return _get_dist(self.agents_pos[0], self.item_pos) < 1 + self.item_size + 0.3
+        return not _in_room(self.agents[0], self.room)
 
 
     def get_reward(self):
-        """get reward"""
         get_item = 1.0 if _get_dist(self.agents_pos[0], self.item_pos) < 1 + self.item_size + 0.3 else 0.0
-        return [get_item, -get_item]
+        return [get_item, 0]
 
 
     def render(self):
@@ -258,10 +256,9 @@ class Guarding_v0:
             0: (0, 0, 0, 255), # ground body
             1: (255, 0, 0, 255), # agent 1
             2: (0, 255, 0, 255), # agent 2
-            3: (0, 0, 255, 255), # item
         }
         self.screen.fill((255, 255, 255, 255))
-        for body_id, body in enumerate([self.room, self.agents[0], self.agents[1]], self.item):
+        for body_id, body in enumerate([self.room, self.agents[0], self.agents[1]]):
             for fixture in body.fixtures:
                 fixture.shape.draw(self.screen, body, fixture, colors[body_id], self.PPM, self.SCREEN_HEIGHT)
         pygame.display.flip()
@@ -289,3 +286,24 @@ class Guarding_v0:
         self.agents = []
         if self.enable_renderer:
             pygame.quit()
+
+
+class Avoiding_v1(Avoiding_v0):
+    """agent 2 blocks agent 1, discrete action"""
+    def __init__(self, PPM=20.0, 
+                       TARGET_FPS=60,
+                       SCREEN_WIDTH=640,
+                       SCREEN_HEIGHT=480,
+                       TIME_STEP=5,
+                       enable_renderer=True,
+                       random_pos=False,
+                       restitution=0):
+        super(Avoiding_v1, self).__init__(PPM, TARGET_FPS, SCREEN_WIDTH, SCREEN_HEIGHT, TIME_STEP, enable_renderer, random_pos, restitution)
+        if self.enable_renderer:
+            pygame.display.set_caption('Avoiding_v1')
+
+
+    def start(self):
+        """start the episode"""
+        self.running = True
+        self.agents_pos = [_get_world_pos(agent) for agent in self.agents]
